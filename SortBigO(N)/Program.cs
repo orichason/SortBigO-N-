7d02fdc818c110c70a08657036a317dbea533414
Ori Chason <orichason@gmail.com>
2023-03-16 16:21:35 -0700

    finished to work with values that are max 2 digits long

--- SortBigO(N)/Program.cs
+++ SortBigO(N)/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SortBigO_N_
@@ -15,6 +16,7 @@ namespace SortBigO_N_
 
             int longestDigit = 0;
             
+            
             for (int i = 0; i < nums.Length; i++)
             {
                 int currentLongestDigit = nums[i].ToString().Length;
@@ -25,19 +27,69 @@ namespace SortBigO_N_
                 }
             }
 
-            for (int i = longestDigit; i > 0; i++)
+            //for(int z = longestDigit; longestDigit >)
+            for (int i = 0, count = 0; i < 10; i++)
             {
-                for(int j = 0; j < )
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] % 10 == i)
+                    {
+                        result[count] = nums[j];
+                        count++;
+                    }
+                }
             }
+            nums = result;
+            int[] tensArray = new int[nums.Length];
+            for(int i = 0, count = 0; i < 10; i++)
+            {
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j]/10 % 10 == i)
+                    {
+                        tensArray[count] = nums[j];
+                        count++;
+                    }
+                }
+            }
+
+            result = tensArray;
+            //int[] hundrethsArray = new int[nums.Length];
+
+            //for(int i = 0, count = 0; i < 10; i++)
+            //{
+            //    for(int j = 0; j < nums.Length; j++)
+            //    {
+            //        int temp = tensArray[j];
+                  
+            //        while (temp >= 10)
+            //        {
+            //            temp /= 10;
+            //        }
+            //        if (temp == i)
+            //        {
+            //            hundrethsArray[count] = tensArray[j];
+            //            count++;
+            //        }
+            //    }
+            //}
 
             return result;
         }
 
         static void Main(string[] args)
         {
-            int[] nums = { 1, 444, 5, 32, 90 };
+            int[] nums = new int[100];
+            Random random= new Random();
 
-            Sort(nums);
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = random.Next(1, 100);
+            }
+
+            nums = Sort(nums);
+
+            ;
         }
     }
 }
